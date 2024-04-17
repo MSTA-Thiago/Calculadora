7ad8d1c4a601a94b527a3516b1fdefb72a4fcc5c
Thiago <167051041+MSTA-Thiago@users.noreply.github.com>
2024-04-17 19:09:47 -0300

    Update flask_app.py4

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,6 +1,6 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask, render_template, request
-#from tarifas_aneel import tarifa_atual
+from tarifas_aneel import tarifa_atual
 from calculo import InputConsumoHFP, InputConsumoHP, InputDemandaHFP, InputDemandaHP, do_calculation
 
 # from InterfaceVisual import interface_visual
