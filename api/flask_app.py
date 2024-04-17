c97f35ec4af8c39c03a3756cc5efb11bd0141820
Thiago <167051041+MSTA-Thiago@users.noreply.github.com>
2024-04-17 17:42:19 -0300

    api final

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,6 +1,5 @@
 # A very simple Flask Hello World app for you to get started with...
 from flask import Flask, render_template, request
-
 from tarifas_aneel import tarifa_atual
 from calculo import InputConsumoHFP, InputConsumoHP, InputDemandaHFP, InputDemandaHP, do_calculation
 
