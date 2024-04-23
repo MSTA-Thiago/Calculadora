21f2e9ac64d9c7bde3caad18419531b43e7011a5
Thiago <167051041+MSTA-Thiago@users.noreply.github.com>
2024-04-23 18:57:30 -0300

    att grafico

--- api/flask_app.py
+++ api/flask_app.py
@@ -1,18 +1,22 @@
-from flask import Flask, render_template, request, Response
 from io import BytesIO
+from flask import Flask, render_template, request, Response
 from reportlab.lib.pagesizes import landscape, A4
-from reportlab.pdfgen import canvas
 from reportlab.lib.units import inch
-from reportlab.lib.styles import getSampleStyleSheet
-from reportlab.lib import colors
-from reportlab.graphics.shapes import Drawing
-from reportlab.graphics.charts.barcharts import VerticalBarChart
+from reportlab.pdfgen import canvas
+import matplotlib
+
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import tempfile
+import os
+import locale
 from api.calculadora import tarifa_atual, do_calculation
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
 form_data = {}
+
 
 @app.route('/', methods=["GET", "POST"])
 def home():
@@ -49,11 +53,14 @@ def store_form_data():
         form_data = request.form.to_dict()
         return 'Dados do formulário armazenados com sucesso.'
 
+
 @app.route('/gerar_pdf', methods=['POST'])
 def gerar_pdf():
     global form_data
+    locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
     if request.method == "POST":
         try:
+            # Imputs:
             distribuidora = form_data["distribuidora"]
             subgrupo = form_data["subgrupo"]
             modalidade = form_data["InputModal"]
@@ -68,9 +75,56 @@ def gerar_pdf():
             preco_pmt = float(form_data["preco_pmt"])
             razao_social = form_data["conta"]
             cnpj = form_data["cnpj"]
-            result = do_calculation(t_atual, InputDemandaHP, InputDemandaHFP, InputConsumoHP, InputConsumoHFP, ICMS, PASEP,
+            result = do_calculation(t_atual, InputDemandaHP, InputDemandaHFP, InputConsumoHP, InputConsumoHFP, ICMS,
+                                    PASEP,
                                     preco_pmt, modalidade)
             total_livre, total_cativo, desconto, total_desconto = result
+            # Gerar o grafico
+            # Cria um array para o eixo X
+            categorias = ['Total Cativo', 'Total Livre + Desconto']
+
+            # Cria o gráfico de barras
+            fig: object
+            fig, ax = plt.subplots(figsize=(10, 8))
+
+            # Adiciona as barras ao gráfico
+            barra_cativo = ax.bar(categorias[0], total_cativo, color='green', label='Total Cativo')
+            barra_livre = ax.bar(categorias[1], total_livre, color='orange', label='Total Livre')
+            barra_desconto = ax.bar(categorias[1], total_desconto, bottom=total_livre, color='white', edgecolor='black',
+                                    linestyle='--', linewidth=2, label='Total Desconto')
+
+            # Função para adicionar os valores formatados dentro das barras
+            def adicionar_valores_dentro_das_barras():
+                # Adiciona o valor formatado dentro da barra de Total Cativo
+                ax.text(barra_cativo[0].get_x() + barra_cativo[0].get_width() / 2, barra_cativo[0].get_height() / 2,
+                        locale.currency(total_cativo, grouping=True), ha='center', va='center', color='black',
+                        weight='bold')
+
+                # Adiciona o valor formatado dentro da barra de Total Livre
+                ax.text(barra_livre[0].get_x() + barra_livre[0].get_width() / 2, barra_livre[0].get_height() / 2,
+                        locale.currency(total_livre, grouping=True), ha='center', va='center', color='black',
+                        weight='bold')
+
+                # Adiciona o valor formatado dentro da barra de Total Desconto
+                ax.text(barra_desconto[0].get_x() + barra_desconto[0].get_width() / 2,
+                        total_livre + barra_desconto[0].get_height() / 2,
+                        locale.currency(total_desconto, grouping=True), ha='center', va='center', color='black',
+                        weight='bold')
+
+            # Chama a função para adicionar os valores formatados dentro das barras
+            adicionar_valores_dentro_das_barras()
+
+            # Adiciona rótulos dos eixos e título
+            ax.set_xlabel('Categorias')
+            ax.set_ylabel('Valor (R$)')
+            ax.set_title('Comparação dos Totais')
+            ax.legend()
+
+            # Salva o gráfico em um arquivo temporário
+            with tempfile.NamedTemporaryFile(delete=False, suffix='.png') as tmpfile:
+                plt.savefig(tmpfile.name)
+                tmpfile_path = tmpfile.name
+
             # Arredondando os valores para duas casas decimais
             total_livre = round(total_livre, 2)
             total_cativo = round(total_cativo, 2)
@@ -88,64 +142,55 @@ def gerar_pdf():
             pagesize = landscape(A4)
             c = canvas.Canvas(buffer, pagesize=pagesize)
 
-            buffer = BytesIO()
-
-
-            # Criação do canvas para o PDF
-            pagesize = landscape(A4)
-            c = canvas.Canvas(buffer, pagesize=pagesize)
-
             # Título
             title_font = 'Helvetica-Bold'
             title_color = '#556B2F'
             title_text = "PROPOSTA PREÇO FIXO"
+
+            # Calcula a posição y para o título
+            title_width = c.stringWidth(title_text, title_font)
+            title_x = (pagesize[0] - title_width) / 2
+            title_y = pagesize[1] - inch  # 1 inch (72 pixels) de espaçamento
+
+            # Define o tamanho da fonte e a cor do título
             c.setFont(title_font, 14)
             c.setFillColor(title_color)
-            title_width = c.stringWidth(title_text)
-            title_x = (pagesize[0] - title_width) / 2
-            title_y = pagesize[1] - 50
             c.drawString(title_x, title_y, title_text)
 
-            # Demais textos
-            normal_font = 'Helvetica'
-            normal_color = 'Black'
-            normal_size = 12
-            c.setFont(normal_font, normal_size)
-            c.setFillColor(normal_color)
-            c.drawString(100, title_y - 50, "Razão social: " + razao_social)
-            c.drawString(100, title_y - 70, "CNPJ: " + cnpj)
-            c.drawString(100, title_y - 90, result_string1)
-            c.drawString(100, title_y - 110, result_string2)
-            c.drawString(100, title_y - 130, result_string3)
-            c.drawString(100, title_y - 150, result_string4)
+            # Define o estilo para os demais textos
+            c.setFont('Helvetica', 12)
+            c.setFillColor('Black')
 
-            # Adicionando o gráfico de barras
-            drawing = Drawing(400, 200)
-            data = [total_cativo, total_livre, total_desconto]
-            categories = ['Total Cativo', 'Total Livre', 'Total Desconto']
-            bc = VerticalBarChart()
-            bc.x = 50
-            bc.y = 50
-            bc.height = 150
-            bc.width = 300
-            bc.data = [data]
-            bc.strokeColor = colors.black
-            bc.bars[0].fillColor = colors.blue
-            bc.categoryAxis.categoryNames = categories
-            bc.categoryAxis.labels.angle = 30
-            bc.valueAxis.valueMin = 0
-            bc.valueAxis.valueMax = max(data) * 1.1
-            drawing.add(bc)
-            drawing.drawOn(c, 100, 150)  # Posição ajustada para o gráfico
+            # Adiciona os rótulos como subtítulos
+            c.setFont('Helvetica-Bold', 12)
+            c.drawString(100, title_y - 50, "Razão social:")
+            c.drawString(100, title_y - 70, "CNPJ:")
+            c.drawString(100, title_y - 90, "Total Livre:")
+            c.drawString(100, title_y - 110, "Total Cativo:")
+            c.drawString(100, title_y - 130, "Desconto:")
+            c.drawString(100, title_y - 150, "Total do desconto:")
 
-            # Salvar o canvas como PDF
+            # Adiciona os valores correspondentes
+            c.setFont('Helvetica', 12)
+            c.drawString(182, title_y - 50, razao_social)
+            c.drawString(140, title_y - 70, cnpj)
+            c.drawString(170, title_y - 90, str(locale.currency(total_livre, grouping=True)))
+            c.drawString(177, title_y - 110, str(locale.currency(total_cativo, grouping=True)))
+            c.drawString(162, title_y - 130, f"{str(desconto)}%")
+            c.drawString(213, title_y - 150, str(locale.currency(total_desconto, grouping=True)))
+
+            # Insere o gráfico no PDF
+            c.drawImage(tmpfile_path, 60, 40, width=400, height=332)
+
+            c.showPage()
             c.save()
             # Retorna o buffer
             buffer.seek(0)
-
+            os.remove(tmpfile_path)
             return Response(buffer.read(), content_type='application/pdf')
         except Exception as e:
             return f'Erro ao gerar o PDF: {str(e)}', 400
+
 
 if __name__ == "__main__":
     app.run(debug=True)
